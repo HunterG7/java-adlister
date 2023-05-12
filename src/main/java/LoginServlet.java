1c6af790ea046c41da8b1edfa5ef587227b8bc09
Hunter Grubbs <hunter.grubbs07@gmail.com>
2023-05-12 15:01:48 -0500

    session and cookies exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,20 +3,32 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/login.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        boolean isLoggedIn = session.getAttribute("username") != null;
+        if (!isLoggedIn) {
+            response.sendRedirect("/profile");
+            return;
+        }
+
+        request.getRequestDispatcher("/profile.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+        session.setAttribute("username", request.getParameter("username"));
+        session.setAttribute("password", request.getParameter("password"));
+        String username = (String) session.getAttribute("username");
+        String password = (String) session.getAttribute("password");
+        boolean validLogin = username != null && username.trim() != "" && password != null && password.trim() != "";
 
-        if (validAttempt) {
+        if (validLogin) {
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
